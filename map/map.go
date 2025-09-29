d15ddf5bbbe585a607dfb1527c172380f0b68526
narvdeshwar <sam.07soni@gmail.com>
2025-09-29 22:46:21 +0530

    basic of map

---
+++ map/map.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	// creation of map
+	m := make(map[string]int)
+
+	// adding values to map
+	m["alice"] = 23
+	m["bob"] = 34
+
+	for index, key := range m {
+		fmt.Println(index, "->", key)
+	}
+	fmt.Println(m["ashrith"]) // it returns 0 when the key or value doesn;t exits
+}
