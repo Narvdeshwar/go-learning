4225b3cb69858cb7887e691b10611599ae31bb3a
narvdeshwar <sam.07soni@gmail.com>
2025-10-01 13:24:00 +0530

    map completed

--- map/map.go
+++ map/map.go
@@ -13,5 +13,14 @@ func main() {
 	for index, key := range m {
 		fmt.Println(index, "->", key)
 	}
-	fmt.Println(m["ashrith"]) // it returns 0 when the key or value doesn;t exits
+	// fmt.Println(m["ashrith"]) // it returns 0 when the key or value doesn;t exits
+
+	// checking any value exits in map or not
+	val, exits := m["ashrith"]
+	if exits {
+		fmt.Println("current value", val)
+	} else {
+		fmt.Println("current value doesn't exits", val)
+	}
+
 }
