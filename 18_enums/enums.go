bcfacd2c28819bb0d8b2ca35b1b7361cc2231c56
narvdeshwar <sam.07soni@gmail.com>
2025-10-28 23:08:45 +0530

    enums example

--- 18_enums/enums.go
+++ 18_enums/enums.go
@@ -2,41 +2,18 @@ package main
 
 import "fmt"
 
-type orderStatus int
+type Day string
 
 const (
-	pending orderStatus = iota
-	received
-	confirmed
-	preparing
-	shipped
-	delivered
+	Sunday    Day = "Sunday"
+	Mondey    Day = "Monday"
+	Tuesday   Day = "Tuesday"
+	Wednesday Day = "Wednesday"
+	Thursday  Day = "Thursday"
+	Friday    Day = "Friday"
+	Saturday  Day = "Saturday"
 )
 
-func (o orderStatus) String() string {
-	return [...]string{"pending", "received", "confirmed", "preparing", "shipped", "delivered"}[o]
-}
-
-func nextStatus(status orderStatus) orderStatus {
-	switch status {
-	case pending:
-		return received
-	case received:
-		return confirmed
-	case confirmed:
-		return preparing
-	case preparing:
-		return shipped
-	case shipped:
-		return delivered
-	default:
-		return status
-	}
-}
-
 func main() {
-	order := pending
-	fmt.Print(order)
-	order = nextStatus(order)
-	fmt.Println(order)
+	fmt.Println(Mondey)
 }
--- 18_enums/enums.md
+++ 18_enums/enums.md
@@ -124,3 +124,26 @@ func main() {
 }
 
 ```
+
+```go
+package main
+
+import "fmt"
+
+type Day string
+
+const (
+	Sunday    Day = "Sunday"
+	Mondey    Day = "Monday"
+	Tuesday   Day = "Tuesday"
+	Wednesday Day = "Wednesday"
+	Thursday  Day = "Thursday"
+	Friday    Day = "Friday"
+	Saturday  Day = "Saturday"
+)
+
+func main() {
+	fmt.Println(Mondey)
+}
+
+```
